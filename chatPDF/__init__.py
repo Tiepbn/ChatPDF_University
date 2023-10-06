e71b2596d5e9ab024269b420d831251048e4d79d
khuyen293 <hoangthikhuyen293@gmail.com>
2023-10-06 18:14:57 +0700

    project_ck

---
+++ chatPDF/__init__.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+app = Flask(__name__)
+# app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///user.db"
+# app.config['SECRET_KEY'] = '8d995ee2e1b43236aaea002c'
+# db = SQLAlchemy(app)
+
+# login_manager = LoginManager(app)
+
+from chatPDF import routes
---
+++ chatPDF/routes.py
@@ -0,0 +1,16 @@
+from chatPDF import app 
+from flask import render_template, redirect, url_for
+# from chatPDF.form import RegisterForm
+
+@app.route('/')
+@app.route('/home')
+def home():  
+    return render_template('home_chatbot.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+@app.route('/sigin')
+def sigin():
+    return render_template('sigin.html')
+
