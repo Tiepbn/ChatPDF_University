3365c3fd8e9cdd8a6905931643131f3ef6516a3b
khuyen293 <hoangthikhuyen293@gmail.com>
2023-10-30 16:01:23 +0700

    project

--- chatPDF/routes.py
+++ chatPDF/routes.py
@@ -1,5 +1,5 @@
 from chatPDF import app 
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, request
 # from chatPDF.form import RegisterForm
 
 @app.route('/')
@@ -23,8 +23,31 @@ def baseuser():
 def usernotaccount():
     return render_template('usernotaccount.html')
 
-@app.route('/admin')
+@app.route('/admin', methods=['GET', 'POST'])
 def admin():
+    if request.method == 'POST':
+        pdf = request.files['pdf']
+        if pdf:
+            pdf_reader = PdfReader(pdf)
+            text = ""
+            for page in pdf_reader.pages:
+                text += page.extract_text()
+
+            text_splitter = RecursiveCharacterTextSplitter(
+                chunk_size=1000,
+                chunk_overlap=200,
+                length_function=len
+            )
+            chunks = text_splitter.split_text(text=text)
+
+            embeddings = OpenAIEmbeddings()
+            VectorStore = FAISS.from_texts(chunks, embedding=embeddings)
+            store_name = pdf.filename[:-4]
+            
+            with open(f"{store_name}.pkl", "wb") as f:
+                pickle.dump(VectorStore, f)
+            
+            return "File processed successfully!"
     return render_template('admin.html')
 
 
